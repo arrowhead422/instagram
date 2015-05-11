69ebe9650a9a59cc696d5eafadb6f3f576a506f8
heroes618 <heroes618@hotmail.com>
2015-05-11 10:18:54 -0700

    video 1 and 2

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// configuration for our php sever
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//make constants using define.
+define('client_Id', '772cefd08fdb49de9716fb8a9e8b75cf');
+define('client_Secret', 'a51cb2f090164afbbd241708343577f3');
+define('redirectURI', 'http://localhost/appacademyapi/index.php');
+define('ImageDirectory', 'pics/');
+//define act like a globe varible
+?>
+
+
+ <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>	
